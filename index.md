8cf0fa9f9d4f1ee417cb3bfdfd61f99e30ab2685
remarkies <luka@ichbinich.ch>
2019-12-13 14:45:24 +0100

    test

--- index.md
+++ index.md
@@ -1,5 +1,10 @@
 ---
 layout: front
-title: "LUC LE GRAND - CODING"
+title: "LUKA"
 subtitle: "I.BA_222_GENCG.H1901 - Winter 2019"
+
+includes:
+  - src: "hole.js"
+  - src: "particle.js"
+  - src: "sketch.js"
 ---
--- sketch.js
+++ sketch.js
@@ -1,83 +1,60 @@
-// Noise generated circle
-
 // Global var
-// Some of the var might be initialised in gui.js
-var canvas, backgroundGrey, radius;
-var actRandomSeed, count, points, increment;
+let productionMode = false;
+let showFPS = false;
+
+let particles = [];
+let holes = [];
+let holesCount = 0;
 
 function setup() {
   // Canvas setup
-  canvas = createCanvas(windowWidth, windowHeight);
+  if(productionMode)
+    canvas = createCanvas(windowWidth, windowHeight-45);
+  else {
+    canvas = createCanvas(windowWidth, windowHeight);
+  }
   canvas.parent("p5Container");
   // Detect screen density (retina)
-  // Comment it out if the sketch is too slow
   var density = displayDensity();
   pixelDensity(density);
-  // Init var
-  // some of the var are initialised in gui.js
-  backgroundGrey = 0;
-  count = 150;
-  points = [count];
-  background(backgroundGrey);
-  radius = 20;
-  increment = +1;
+  frameRate(60);
+
+  for(let i = 0; i < height; i+=2) {
+    particles.push(new Particle(createVector(0, i)));
+  }
+
+  for(let j = 0; j < holesCount; j++) {
+    holes.push(new Hole(createVector(random(width), random(height)), random(3, 10)));
+  }
+
+  background(0);
 }
 
 function draw() {
-  // background(backgroundGrey, 20);
-  smooth();
 
-  // Create points array
-  let faderX = .1;
-  let t = millis()/1000;
-  // let r = map(mouseY,0,height,10,radius);
-  if (radius>width/1.7 && radius>height/1.7) increment = -increment;
-  else if (radius<20) increment = -increment;
-  radius += increment; 
-  let angle = radians(360/count);
+  particles.forEach(particle => {
+    particle.flow();
+    particle.display();
+  })
 
-  for (let i=0; i<count; i++){
-    let radiusRand = radius - noise(t, i*faderX)*50;
-    let x = width/2 + cos(angle*i)*radiusRand;
-    let y = height/2 + sin(angle*i)*radiusRand;
-    points[i] = createVector(x,y);
-  }
+  holes.forEach(hole => {
+    hole.display();
+  });
 
-  // Draw
-  // stroke(noise(t/10)*255,0,noise(t/1)*100,255);
-  strokeHsluv(noise(t/10)*360,noise(t/20)*50,noise(t)*80);
-  strokeWeight(20);
-  noFill();
-  beginShape();
-  for (let i=0; i<count; i++){
-    // fill(255);
-    // ellipse(points[i].x, points[i].y,2,2);
-    // noFill();
-    curveVertex(points[i].x, points[i].y);
-    if (i==0 || i==count-1) curveVertex(points[i].x, points[i].y);
-  }
-  endShape(CLOSE);
+  if(showFPS)
+    displayFPS();
+}
+
+function displayFPS() {
+  let fps = frameRate();
+  fill(255);
+  stroke(0);
+  textSize(window.innerHeight / 50);
+  text("FPS: " + fps.toFixed(2), 10, height - 10);
 }
 
 function keyPressed() {
-  if (key == DELETE || key == BACKSPACE) background(360);  
   if (key == 's' || key == 'S') saveThumb(650, 350);
-}
-
-// Color functions
-function fillHsluv(h, s, l) {
-  var rgb = hsluv.hsluvToRgb([h, s, l]);
-  fill(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
-}
-
-function strokeHsluv(h, s, l) {
-  var rgb = hsluv.hsluvToRgb([h, s, l]);
-  stroke(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
-}
-
-function colorHsluv(h, s, l) {
-  var rgb = hsluv.hsluvToRgb([h, s, l]);
-  return color(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
 }
 
 // Tools
@@ -87,7 +64,7 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight, false);
 }
 
-//  conversion
+// Int conversion
 function toInt(value) {
   return ~~value;
 }
@@ -99,6 +76,6 @@ function timestamp() {
 
 // Thumb
 function saveThumb(w, h) {
-  let img = get(width / 2 - w / 2, height / 2 - h / 2, w, h);
-  save(img, 'thumb.jpg');
+  let img = get( width/2-w/2, height/2-h/2, w, h);
+  save(img,'thumb.jpg');
 }
